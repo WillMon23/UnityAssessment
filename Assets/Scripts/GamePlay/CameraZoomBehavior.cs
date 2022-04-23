5238162ed52143ba474efbfd5122b2ef6eadfc70
Will <montero_w@live.com>
2022-04-23 16:06:55 -0500

    Camera Zoom and HEalthBar
    Completed Comments for these classes

--- Assets/Scripts/GamePlay/CameraZoomBehavior.cs
+++ Assets/Scripts/GamePlay/CameraZoomBehavior.cs
@@ -5,11 +5,16 @@ using System;
 
 public class CameraZoomBehavior : MonoBehaviour
 {
+    //gets a gets a refrence to the camera  
     private Camera _camera;
+    //gets the use wanted aspect for the camera 
     [SerializeField]
     private Vector2 _refrenceAspectRatio;
+    //Kepts track of the current camera starting position
     private Vector3 _startPos;
+    //the value of the aspect of the camera devide by the x by the y 
     private float _refRatio;
+    //sets the scale of the zoom 
     [SerializeField] Vector3 _zoomScale = Vector3.one;
 
     // Start is called before the first frame update
@@ -23,11 +28,13 @@ public class CameraZoomBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Callculates the the ratio 
         double ratio = _refRatio / _camera.aspect;
         ratio = Math.Round(ratio, 4);
 
+        //Callculates the the xoom based of the starting position and the sets scalining 
         Vector3 scaledPosition = Vector3.Scale(_startPos * (float)ratio,_zoomScale);
-
+        //Sets the camera position based off it's scaled position 
         transform.position = scaledPosition;
     }
 }
--- Assets/Scripts/GamePlay/HealthBarBehaviro.cs
+++ Assets/Scripts/GamePlay/HealthBarBehaviro.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class HealthBarBehaviro : MonoBehaviour
 {
+    //To get the refence of the slider 
     private Slider _slider;
+    //The player health value 
     [SerializeField]
     private HealthBehavior _playerHeath;
 
     // Start is called before the first frame update
     void Awake()
     {
+        //Gets the refrence of the slider 
         _slider = GetComponent<Slider>();
         _slider.maxValue = _playerHeath.Health;
     }
@@ -19,6 +22,7 @@ public class HealthBarBehaviro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Updates the value from the player tp the slider
         _slider.value = _playerHeath.Health;
     }
 }
